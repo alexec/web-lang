77402559b759aaca953ab247e80ccacc98970f3e
Alex Collins <alex.e.c@gmail.com>
2016-03-26 12:08:57 +0000

    removed slf4j

--- src/main/java/wl/domain/Journey.java
+++ src/main/java/wl/domain/Journey.java
@@ -1,13 +1,11 @@
 package wl.domain;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@Slf4j
 public class Journey {
     private String name;
     private List<Step> steps = new ArrayList<>();
